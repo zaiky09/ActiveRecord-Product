ae3d38292d4599a5efa821c2a1e352f68bb9766f
zaiky09 <zamilmozamil@outlook.com>
2023-02-27 19:07:51 +0300

    Final touches

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
 
-    def fav_product
+    def favorite_product
         reviews.max_by {|rev| rev.star_rating}.product
     end
 
-    def chuck_reviews(product)
+    def remove_reviews(product)
         Review.where(product_id: product.id, user_id: self.id).destroy_all
     end
 
